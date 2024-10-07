210baeb787d4956837b84226bd7d9e3a74d551ee
Phan Van Minh <21522345@gm.uit.edu.vn>
2024-10-07 16:49:26 +0700

    first commit

---
+++ src/main/java/com/demo/ecommerce/controller/ImageController.java
@@ -0,0 +1,40 @@
+package com.demo.ecommerce.controller;
+import com.demo.ecommerce.service.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/image")
+public class ImageController {
+    @Autowired
+    private StorageService service;
+
+    // Test
+    @GetMapping("/")
+    public String test(){
+        return "test upload image";
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<?> uploadImage(@RequestParam("image") MultipartFile file) throws IOException {
+        String uploadImage = service.uploadImage(file);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(uploadImage);
+    }
+
+    @GetMapping("/{fileName}")
+    public ResponseEntity<?> downloadImage(@PathVariable String fileName){
+        byte[] imageData=service.downloadImage(fileName);
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.valueOf("image/png"))
+                .body(imageData);
+
+    }
+}
